00ef95c1709e61c4baafca7224f15e475638dc5f
maybeShuo <546537825@qq.com>
2017-06-14 23:48:40 +0800

    init.

---
+++ src/mg/components/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    constructor (props) {
+        super(props);
+
+    }
+
+    static defaultProps = {
+
+    }
+
+    static propTypes = {
+
+    }
+
+    state = {
+
+    }
+
+    componentDidMount()
+    {
+
+    }
+
+    render()
+    {
+
+        return (
+            <div className="game-app">
+                连连看
+            </div>
+        );
+    }
+}
---
+++ src/mg/index.js
@@ -0,0 +1,15 @@
+import './less/index';
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './components/App';
+
+function run()
+{
+    render(
+        <App />,
+        document.getElementById("root")
+    );
+}
+
+run();
